36c506c3ca5dfe36c063ac164b20d92de7ac2fdf
shao0408 <42964187+shao0408@users.noreply.github.com>
2018-12-10 10:19:42 +0800

    优化

--- REST-CSharp-demo/Huobi.Rest.CSharp.DemoTests/HuobiApiTests.cs
+++ REST-CSharp-demo/Huobi.Rest.CSharp.DemoTests/HuobiApiTests.cs
@@ -4,47 +4,47 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Linq;
 
-//accessKey:  
-//secretKey:  
+
 namespace Huobi.Rest.CSharp.Demo.Tests
 {
     [TestClass()]
     public class HuobiApiTests
-    {
-		//accessKey:  
-		//secretKey: 
-        HuobiApi api = new HuobiApi("XXXXXXXXXXX", "XXXXXXXXXXX");
-        [TestMethod()]
-        public void GetAllAccountTest()
-        {
-            var result = api.GetContractInfo();
-            Assert.IsNotNull(result);
-        }
+    {	//accessKey: 
+		//secretKey:  
+        HuobiApi api = new HuobiApi("XXXXX", "XXXXX");
+          [TestMethod()]
+          public void GetAllAccountTest()
+          {
+          var result = api.GetContractInfo();
+          Assert.IsNotNull(result);
+         }
 
         [TestMethod()]
         public void OrderPlaceTest()
         {
 
             OrderPlaceRequest req = new OrderPlaceRequest();
-            req.volume = 1;
+            req.volume = "1";
             req.direction = "buy";
-            req.price = 10;
+            req.price = "100";
             req.offset = "open";
-            req.lever_rate = 10;
+            req.lever_rate = "10";
+            req.contract_code = "BTC181214";
             req.order_price_type = "limit";
-            var result = api.OrderPlace(req);
-            Assert.AreEqual(result.Status, "ok");
-        }
-
-        [TestMethod()]
-        public void OrderPositionTest()
-        {
-
-            OrderPositionRequest req = new OrderPositionRequest();
             req.symbol = "BTC";
-            
-            var result = api.OrderPosition(req);
-            Assert.AreEqual(result.Status, "ok");
+            req.contract_type = "this_week";
+            var result = api.OrderPlace(req);
+           Assert.AreEqual(result.Status, "ok");
         }
+
+         [TestMethod()]
+          public void OrderPositionTest()
+         {
+         OrderPositionRequest req = new OrderPositionRequest();
+         req.symbol = "BTC";
+
+           var result = api.OrderPosition(req);
+          Assert.AreEqual(result.Status, "ok");
+          }
     }
 }
